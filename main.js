439b34d385ce556eebe207f110b110435e90a6f4
Matías <matiascerolenii@gmail.com>
2022-06-10 19:10:27 -0300

    updated

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-$(document).ready(function(){
-    $(".sub-btn").click(function(){
-        $(this).next(".sub-menu").slideToggle();
-    });
-});
-//javascript  abrir,cerrar menú
+//abrir,cerrar sidebar
 var menu = document.querySelector(".menu");
 var menuBtn = document.querySelector(".menu-btn");
 var closeBtn = document.querySelector(".close-btn");
+var overlayNav = document.querySelector(".nav-overlay");
 
 menuBtn.addEventListener("click", () => {
     menu.classList.add("active");
@@ -15,42 +11,57 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     menu.classList.remove("active");
 });
+overlayNav.addEventListener("click", () => {
+    menu.classList.remove("active");
+});
+
+
+
+// Submenu contato
+$(document).ready(function () {
+    $(".sub-btn").click(function () {
+        $(this).next(".sub-menu").slideToggle();
+    });
+});
+
 
 //javascript efecto scroll
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+
 // carrusel
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
         dist: -150,
     });
-  });
+});
 
 
-    //   autoplay carrusel
-    setInterval(function(){
-        $('.carousel').carousel('next');
-    },3000);
+//   autoplay carrusel
+setInterval(function () {
+    $('.carousel').carousel('next');
+}, 3000);
 
-    // boton ir arriba flotante
-    $(document).ready(function(){
 
-        $('#ir-arriba').click(function(){
-            $('body, html').animate({
-                scrollTop: '0px'
-            }, 300);
-        });
-    
-        $(window).scroll(function(){
-            if( $(this).scrollTop() > 0 ){
-                $('#ir-arriba').slideDown(300);
-            } else {
-                $('#ir-arriba').slideUp(300);
-            }
-        });
-    
+// boton ir arriba flotante
+$(document).ready(function () {
+
+    $('#ir-arriba').click(function () {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#ir-arriba').slideDown(300);
+        } else {
+            $('#ir-arriba').slideUp(300);
+        }
+    });
+
+});
